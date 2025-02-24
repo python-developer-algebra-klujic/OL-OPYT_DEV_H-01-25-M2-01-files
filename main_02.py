74cd7a0db6580f1ed87e0e36907ca92ec747e193
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-02-24 21:08:07 +0100

    Update main_02.py

--- main_02.py
+++ main_02.py
@@ -70,3 +70,24 @@ try:
 except Exception as ex:
     print(f'NIJE MOGUCE POKRENUTI APLIKACIJU')
     print(f'Dogodila se greska {ex}.')
+
+
+
+
+
+'''
+DZ
+
+Kreirati aplikaciju koja omogucava korisniku neogranicen unos proizvoda
+te ih pohranjuje ih u datoteku (naizv datoteke po izboru, ali ucitan iz app_config.ini).
+
+Svaki proizvod treba biti u zasebnoj liniji u datoteci.
+SVaki proizvod treba imati
+ID - redni broj od 1
+Naziv
+Sifru
+Cijenu - valutu NE cuvate u datoteci
+Oznaku valute - ucitajte iz app_config.ini datoteke
+
+Kreirajte sve potrebne funkcije za unos, pohranu i ispis proizvoda te izbornik
+'''
